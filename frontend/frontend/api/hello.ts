7ea7864a04c69676cd69ad16bb9999986d813aaf
Victor Varela <otrovm10@gmail.com>
2026-01-10 05:42:12 -0600

    Ajustes para que Vercel reconozca el proyecto

--- frontend/frontend/api/hello.ts
+++
@@ -1,9 +0,0 @@
-export const config = {
-  runtime: "edge"
-};
-
-export default function handler(req: Request) {
-  return new Response(JSON.stringify({ ok: true }), {
-    headers: { "Content-Type": "application/json" }
-  });
-}
