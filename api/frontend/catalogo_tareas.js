f9e15028a82b3311f60dcfd8a018a3d2f7012b2d
Victor Varela <otrovm10@gmail.com>
2026-01-11 04:56:13 -0600

    Actualización completa: backend y frontend corregidos

--- api/frontend/catalogo_tareas.js
+++
@@ -1,6 +0,0 @@
-import { readJson } from './_utils/readJson.js';
-
-export default async function handler(req, res) {
-  const catalogo = await readJson('catalogo_tareas.json');
-  res.status(200).json(catalogo);
-}
